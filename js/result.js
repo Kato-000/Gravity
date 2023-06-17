9072eeed92d677ec5e3f0cbc385d2ef7b1cbf6f9
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-17 21:44:42 +0900

    new

--- js/result.js
+++ js/result.js
@@ -10,4 +10,4 @@ FruitsName = data["A" + Number].name;
 // 取得した値を必要な処理に適用するなど
 var outputElement = document.getElementById("output");
 console.log(Quantity);
-outputElement.textContent = FruitsName + Quantity + "個分";
+outputElement.textContent = FruitsName + ":" + Quantity + "個分";
