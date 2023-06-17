54560a16a4f0a169f9437ee63b4e25063f870701
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-17 23:50:24 +0900

    new

--- js/fruit_fall.js
+++ js/fruit_fall.js
@@ -44,14 +44,14 @@ function createFallingImage() {
     var windowWidth  = window.innerWidth;
     var windowHeight = window.innerHeight;
     
-    var imageWidth  = Math.floor(Math.random() * 100) + 50; // 50から150の間でランダムな幅を設定
-    var imageHeight = Math.floor(Math.random() * imageCount*2+100) + 100; // 50から550の間でランダムな高さを設定
+    var imageWidth  = Math.floor(Math.random() * 400) + 50; // 50から150の間でランダムな幅を設定
+    var imageHeight = Math.floor(Math.random() * 100) + 400; // 50から550の間でランダムな高さを設定
     
-    image.style.left = Math.floor(Math.random() * (windowWidth -imageWidth)) + 'px'; // ランダムな水平位置を設定
-    image.style.top  = -imageHeight-50  + 'px'; // 画面の上端の外側に初期位置を設定
+    image.style.left = Math.floor(Math.random() * (windowWidth + imageWidth)) - imageWidth + 'px'; // ランダムな水平位置を設定
+    image.style.top  = - imageHeight + 'px'; // 画面の上端の外側に初期位置を設定
     
     document.body.appendChild(image);
-    var posY  = -imageHeight-500; // 初期位置を画面の上端の外側に設定
+    var posY  = - imageHeight - 500; // 初期位置を画面の上端の外側に設定
     var speed = Math.random() * 14 + 5; // 1から20の間でランダムな速度を設定
 
     
@@ -62,9 +62,9 @@ function createFallingImage() {
         if (posY < windowHeight) {
             requestAnimationFrame(fall); // まだ画面内にいる場合は再帰的に呼び出す
         } 
-        if(posY > windowHeight-imageHeight){
+        if(posY > windowHeight - image.height + 20){
             speed = 0;
-            posY = windowHeight-imageHeight;
+            posY = windowHeight - image.height + 20;
         }
     }
     
