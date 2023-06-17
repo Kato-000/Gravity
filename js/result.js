90608cbfac4f0da7d9ccf99d9da4b98c1655b5a0
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-17 22:15:03 +0900

    new

--- js/result.js
+++ js/result.js
@@ -12,7 +12,5 @@ var FruitsExplanation = data["A" + Number].explanation;
 // 取得した値を必要な処理に適用するなど
 var outputElement = document.getElementById("output");
 var outputExplanation = document.getElementById("explanation");
-console.log(Quantity);
 outputElement.textContent = FruitsName + ":" + Quantity + "個分";
 outputExplanation.textContent = FruitsExplanation;
-console.log(FruitsExplanation);
