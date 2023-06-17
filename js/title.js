4d057f63cd229faff288d6e980573431a25826d0
guriguri00451 <guriguri00451@gmail.com>
2023-06-17 17:04:15 +0900

    ssss  Please enter the commit message for your changes. Lines starting

--- js/title.js
+++ js/title.js
@@ -5,10 +5,10 @@ function getValue() {
     var weight = parseFloat(value); // 変数をローカルに宣言
     if(weight){
         sessionStorage.setItem("weight", weight); // 値を sessionStorage に保存
+        location.href = 'measuring.html';
     } else {
         alert("体重を入力してください！")
-        location.href = 'measuring.html';
-    }
+            }
 }
 
 // ページが読み込まれた際に sessionStorage から値を取得
