e17c126c10891566dab3294833a518f331f7f630
jin0818 <morimojin@gmail.com>
2023-06-17 16:02:37 +0900

    tree hyouji

--- test/furuit_fall.js
+++ test/furuit_fall.js
@@ -32,38 +32,44 @@ window.addEventListener('DOMContentLoaded', function() {
 	document.body.style.backgroundAttachment = 'fixed';
 });
 
+const imageFruits = 'fruit_apple.jpg';
+
 function createFallingImage() {
-    var image = new Image();
-    image.src = 'apple.png';
-    image.classList.add('apple');
-    
-    var windowWidth = window.innerWidth;
-    var imageWidth = Math.floor(Math.random() * 100) + 50; // 50から150の間でランダムな幅を設定
-    
-    image.style.left = Math.floor(Math.random() * (windowWidth - imageWidth)) + 'px'; // ランダムな水平位置を設定
-    
-    document.body.appendChild(image);
-    
-    var posY = -image.height; // 初期位置を画面の上端の外側に設定
-    
-    function fall() {
-      posY += 1; // 落下速度を調整する場合は値を変更
-    image.style.top = posY + 'px';
-    
-    if (posY < window.innerHeight) {
-        requestAnimationFrame(fall); // まだ画面内にいる場合は再帰的に呼び出す
-    } else {
-        document.body.removeChild(image); // 画面外に出たら要素を削除
-    }
-    }
-    
-    requestAnimationFrame(fall); // 最初のフレームを呼び出す
+	const image = document.createElement('img');
+	image.scr = imageFruits;
+	image.classList.add('falling-image');
+
+	const windowWidth  = window.innerWidth;
+	const windowHeight = window.innerHeight;
+	image.style.left = `${initialLeft}px`;
+
+	function removeImage(){
+		image.parentNode.removeChild(image);
+	}
+
+	function animateImage(){
+		const windowHeight = window.innerHeight;
+
+		let top = parseInt(image.style.top);
+		if(top >= windowHeight){
+			removeImage();
+		}
+		else{
+			top += 1;
+			image.style.top = `${top}px`;
+
+			requestAnimationFrame(animateImage);
+		}
+	}
+
+	animateImage();
+
+	const imageContainer = document.getElementById('image-container');
+	imageContainer.appendChild(image);
 }
 
-window.addEventListener('DOMContentLoaded', function() {
-    var imageCount = 5; // 落下させる画像の数
-    
-    for (var i = 0; i < imageCount; i++) {
-        createFallingImage();
-    }
-});
+function startFallingEffect(){
+	setInterval(createFallingImage, 500);
+}
+
+window.addEventListener('DOMContentLoaded', startFallingEffect);
