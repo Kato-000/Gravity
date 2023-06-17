6a980118d341ad1719c5b31eb4bed8a4b52ec403
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-17 20:15:19 +0900

    new

--- js/result.js
+++ js/result.js
@@ -1,3 +1,6 @@
+var Quantity = sessionStorage.getItem("Quantity");
+console.log("保存された値: " + Quantity);
+// 取得した値を必要な処理に適用するなど
 var outputElement = document.getElementById("output");
 console.log(Quantity);
 outputElement.textContent = Quantity;
