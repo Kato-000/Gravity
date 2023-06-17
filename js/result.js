9c7148fe03adbbb4a1f4d117a29e1ae8556714ec
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-17 21:53:17 +0900

    new

--- js/result.js
+++ js/result.js
@@ -6,8 +6,11 @@ console.log("保存された値: " + Number);
 
 var data = JSON.parse(Fruites);
 FruitsName = data["A" + Number].name;
+FruitsExplanation = data["A" + Number].explanation;
 
 // 取得した値を必要な処理に適用するなど
 var outputElement = document.getElementById("output");
+var outputExplanation = document.getElementById("explanation");
 console.log(Quantity);
 outputElement.textContent = FruitsName + ":" + Quantity + "個分";
+outputExplanation = FruitsExplanation;
