f8a933f2e2f5ae0de28bceea50d70a8742b6678d
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-17 20:09:39 +0900

    new

--- js/measuring.js
+++ js/measuring.js
@@ -1,9 +1,11 @@
-function NextPage() {
+setTimeout(() => {
+    // 5秒後に実行する処理をここに記述
     var value = (weight * 1000 / FruitsWeight[0]).toFixed(1);;
     var Quantity = parseFloat(value); // 変数をローカルに宣言
     sessionStorage.setItem("Quantity", Quantity); // 値を sessionStorage に保存
     location.href='result.html';
-}
+  }, 5000);
+
 
 // ページが読み込まれた際に sessionStorage から値を取得
 
