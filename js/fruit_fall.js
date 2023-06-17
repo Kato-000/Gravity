096d26feb1a5bb62286af92f72d3d2492be59fd3
jin0818 <morimojin@gmail.com>
2023-06-18 01:20:23 +0900

    imave cut

--- js/fruit_fall.js
+++ js/fruit_fall.js
@@ -44,15 +44,15 @@ function createFallingImage() {
     var windowWidth  = window.innerWidth;
     var windowHeight = window.innerHeight;
     
-    var imageWidth  = Math.floor(Math.random() * 400) + 50; // 50から150の間でランダムな幅を設定
-    var imageHeight = Math.floor(Math.random() * 100) + 400; // 50から550の間でランダムな高さを設定
+    var imageWidth  = Math.floor(Math.random() * image.width) + 50; // ランダムな幅を設定
+    var imageHeight = Math.floor(Math.random() * 100) + 50; // 50から150の間でランダムな高さを設定
     
-    image.style.left = Math.floor(Math.random() * (windowWidth + imageWidth)) - imageWidth + 'px'; // ランダムな水平位置を設定
+    image.style.left = Math.floor(Math.random() * (windowWidth+100 + imageWidth)) - imageWidth -50+ 'px'; // ランダムな水平位置を設定
     image.style.top  = - imageHeight + 'px'; // 画面の上端の外側に初期位置を設定
     
     document.body.appendChild(image);
     var posY  = - imageHeight - 500; // 初期位置を画面の上端の外側に設定
-    var speed = Math.random() * 14 + 5; // 1から20の間でランダムな速度を設定
+    var speed = Math.random() * 15 + 5; // 5から20の間でランダムな速度を設定
 
     
     function fall() {
@@ -62,7 +62,7 @@ function createFallingImage() {
         if (posY < windowHeight) {
             requestAnimationFrame(fall); // まだ画面内にいる場合は再帰的に呼び出す
         } 
-        if(posY > windowHeight - image.height + 20){
+        if(posY > windowHeight - image.height){
             speed = 0;
             posY = windowHeight - image.height + 20;
         }
@@ -75,7 +75,7 @@ function createFallingImage() {
     var imageCount = Quantity; // 落下させる画像の数
     
     for (var i = 0; i < imageCount; i++) {
-    createFallingImage();
+        createFallingImage();
     }
 
     backgroundTree();
