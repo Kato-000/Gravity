bcdea127c5a178ff11bb9391c83f263bf780b98e
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-17 14:49:47 +0900

    new

--- js/title.js
+++ js/title.js
@@ -1,8 +1,9 @@
+var weight;
+
 function getValue() {
-    // HTMLのinput要素にアクセスして値を取得
     var inputElement = document.getElementById("myInput");
     var value = inputElement.value;
-
-    // 取得した値を表示
     console.log("入力された値: " + value);
-  }
+    weight = parseFloat(value);
+    location.href='measuring.html'
+}
