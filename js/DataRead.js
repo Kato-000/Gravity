87ede4fd761a306cc6c2e76404a5bc935b2c9b6a
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-17 15:11:46 +0900

    new

--- js/DataRead.js
+++ js/DataRead.js
@@ -1,9 +1,13 @@
-fetch('../Data/FruitsData.csv')
-  .then(response => response.text())
-  .then(csvData => {
-    print(csvData);
-    // CSVデータを処理するコードをここに記述する
-  })
-  .catch(error => {
-    console.error('CSVファイルの読み込みエラー:', error);
-  });
+let FruitsName = [];
+let FruitsWeight = [];
+let FruitsException = [];
+
+// fetch('../Data/FruitsData.csv')
+//   .then(response => response.text())
+//   .then(csvData => {
+//     print(csvData);
+//     // CSVデータを処理するコードをここに記述する
+//   })
+//   .catch(error => {
+//     console.error('CSVファイルの読み込みエラー:', error);
+//   });
