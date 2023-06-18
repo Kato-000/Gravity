dad3cb95d4498491101b0863d9a3c32ee95ab83b
jin0818 <morimojin@gmail.com>
2023-06-18 15:20:47 +0900

    melon

--- js/DataRead.js
+++ js/DataRead.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-var FruitesNumber = 12;
-=======
 var FruitesNumber = 14;
->>>>>>> 0a24b161bf2b458f7b2ba24effc91c7d2f214c24
 var i = Math.floor(Math.random() * FruitesNumber) + 1;
 var Num = parseFloat(i); // 変数をローカルに宣言
 var data = JSON.parse(Fruits);
