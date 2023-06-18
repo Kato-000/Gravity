b77ccb58b35fd0b1cc618e8e7d9f66c24893634c
jin0818 <morimojin@gmail.com>
2023-06-18 13:41:07 +0900

    title fix

--- js/titleBackground.js
+++ js/titleBackground.js
@@ -1,49 +1,27 @@
-var i = 0, n = 0;
-var FruitesNumber = 7;
-var data = JSON.parse(Fruits);
 
-function createFallingImage() {
+// 画像を表示する関数
+function displayImages() {
+
+for (var i = 0; i < 100; i++) {
 
     var image = new Image();
-    var i = Math.floor(Math.random() * FruitesNumber) + 1;
-    var FruitsImg = data["A" + i].img;
+    var j     = Math.floor(Math.random() * FruitesNumber) + 1;
 
-    image.src = FruitsImg;
-    image.classList.add('fruits');
+    image.src = FruitesImg = data["A" + j].img;
+    image.classList.add('fruit');
 
     var windowWidth  = window.innerWidth;
     var windowHeight = window.innerHeight;
-    
-    var imageWidth  = Math.floor(Math.random() * (windowWidth + image.width)) + 50; // ランダムな幅を設定
-    var imageHeight = Math.floor(Math.random() * (windowHeight + image.height)); // 50から150の間でランダムな高さを設定
-    
-    image.style.left = imageWidth + 'px'; // ランダムな水平位置を設定
-    image.style.top  = imageHeight + 'px'; // 画面の上端の外側に初期位置を設定
-    
+
+    var left = Math.floor(Math.random() * (window.innerWidth + image.width))   - 100;
+    var top  = Math.floor(Math.random() * (window.innerHeight + image.height)) - 100;
+
+    image.style.left = left + 'px';
+    image.style.top  = top  + 'px';
+
+    // 画像をコンテナに追加
     document.body.appendChild(image);
-    var posY  = - imageHeight - 500; // 初期位置を画面の上端の外側に設定
-    var speed = Math.random() * 25 + 10;
-    var randonNum = Math.random() * 100 - 200
-
-    
-    function fall() {
-        posY += speed; // 速度に応じて垂直位置を変化させる
-        image.style.top = posY + 'px';
-        speed = 0;
-        posY = windowHeight - image.height - i - randonNum;
-
-    }
-    
-    requestAnimationFrame(fall); // 最初のフレームを呼び出す
+}
 }
 
-function wait(delay) {
-    return new Promise(resolve => setTimeout(resolve, delay));
-}
-
-window.addEventListener('DOMContentLoaded', async function() {
-    for (; i < 1000; i++) {
-        createFallingImage();
-    }
-});
-
+window.addEventListener('DOMContentLoaded', displayImages);
