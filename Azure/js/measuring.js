af8a300950b8903c72e9e9689dac638f657b0ed3
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-18 09:57:23 +0900

    Azure

---
+++ Azure/js/measuring.js
@@ -0,0 +1,11 @@
+var weight = sessionStorage.getItem("weight");
+console.log("保存された値: " + weight);
+
+setTimeout(() => {
+    // 5秒後に実行する処理をここに記述
+    var value = (weight * 1000 / FruitesWeight).toFixed(1);;
+    var Quantity = parseFloat(value); // 変数をローカルに宣言
+    sessionStorage.setItem("Quantity", Quantity); // 値を sessionStorage に保存
+    location.href='result.html';
+}, 5000);
+
