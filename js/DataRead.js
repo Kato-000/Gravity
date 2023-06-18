42a2fd0fb26a87ef94b87db6c79b24424cda95b3
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-18 10:28:38 +0900

    Data new

--- js/DataRead.js
+++ js/DataRead.js
@@ -1,10 +1,10 @@
-var data = JSON.parse(Fruites);
-
 var FruitesNumber = 5;
 var i = Math.floor(Math.random() * FruitesNumber) + 1;
 var Num = parseFloat(i); // 変数をローカルに宣言
 
-var FruitesName = data["A" + i].name;
-var FruitesWeight = data["A" + i].weight;
-var FruitesImg = data["A" + i].img;
+var data = JSON.parse(Fruites["A" + i]);
+
+var FruitesName = data.name;
+var FruitesWeight = data.weight;
+var FruitesImg = data.img;
 sessionStorage.setItem("Number", Num); // 値を sessionStorage に保存
