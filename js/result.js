e3ca46e5297b33e55c8f904e1985231e0077bfcd
Kato-000 <101191930+Kato-000@users.noreply.github.com>
2023-06-18 13:41:57 +0900

    nwe

--- js/result.js
+++ js/result.js
@@ -26,7 +26,15 @@ else if(Weight < 2.5) outputmemo.textContent = "いい筋トレ道具だね";
 else if(Weight < 3) outputmemo.textContent = "赤ちゃん?";
 else if(Weight < 5) outputmemo.textContent = "小学生のランドセルぐらい";
 else if(Weight < 6) outputmemo.textContent = "ボーリングの玉って人の頭ぐらいらしい";
-
+else if(Weight < 6) outputmemo.textContent = "ボーリングの玉って人の頭ぐらいらしい";
+else if(Weight > 62 && Weight < 68) outputmemo.textContent = "成人女性の平均";
+else if(Weight > 73 && Weight < 83) outputmemo.textContent = "成人男性の平均";
+else if(Weight > 100 && Weight < 150) outputmemo.textContent = "肥満です痩せましょう";
+else if(Weight == 132.7) outputmemo.textContent = "痩せろデブ!";
+else if(Weight > 200 && Weight < 250) outputmemo.textContent = "人間!?";
+else if(Weight < 4300 && Weight > 3800) outputmemo.textContent = "メスのゾウさんですか？";
+else if(Weight < 6300 && Weight > 5800) outputmemo.textContent = "オスのゾウさんですか？";
+else if(Weight > 5971000000000000000000000 && Weight < 5973000000000000000000000) outputmemo.textContent = "地球";
 
 outputExplanation.textContent = FruitsExplanation;
 
